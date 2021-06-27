3075709083ffd6117d9f32345b6e3a455c5136b6
chiraggarg357 <85435902+chiraggarg357@users.noreply.github.com>
2021-06-28 04:43:39 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+  
+  return (
+  <footer>
+    <p>Copyright by ShapeAi @ 2021 {new Date().getFullYear()}</p> 
+    </footer>
+  );
+
+}
+export default Footer;
